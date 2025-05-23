e4bc0baa7ff898adc0abc257073418558c6f84be
ronmaizel <ronmzl2003@gmail.com>
2025-05-23 23:10:49 +0300

    fixed if statements in health check and installation commands

--- start_trial.sh
+++ start_trial.sh
@@ -11,16 +11,17 @@ WEB_PORT=80
 
 # install packages
 echo "Installing necessary packages."
-sudo dnf install curl
+dnf -y install curl
 curl -fsSL https://get.docker.com -o get-docker.sh
+chmod +x get-docker.sh
 ./get-docker.sh
-sudo dnf install -y docker-compose-plugin python3 python3-pip git nfs-common
+dnf install -y docker-compose-plugin python3 python3-pip git nfs-common
 
 # add current user to docker group
-sudo usermod -aG docker $(whoami)
+usermod -aG docker $(whoami)
 
 # open relevant ports
-sudo firewall-cmd --add-port={80,5342,2049,8080}/tcp
+firewall-cmd --add-port={80,5342,2049,8080}/tcp
 
 # clone git repo
 echo "Cloning project repository."
@@ -32,8 +33,8 @@ fi
 
 # mount NFS
 echo "Mounting NFS share."
-sudo mkdir -p "$NFS_MOUNT_PATH"
-sudo mount -t nfs "$NFS_service_IP:$NFS_REMOTE_PATH" "$NFS_MOUNT_PATH"
+mkdir -p "$NFS_MOUNT_PATH"
+mount -t nfs "$NFS_service_IP:$NFS_REMOTE_PATH" "$NFS_MOUNT_PATH"
 echo "$NFS_service_IP:$NFS_REMOTE_PATH $NFS_MOUNT_PATH nfs defaults 0 0" >> /etc/fstab
 
 # start environment
@@ -48,19 +49,16 @@ if curl -s http://localhost:$WEB_PORT | grep -q '200'; then
     echo "Nginx web service is running."
 else
     echo "Nginx web service is not responding. Check container logs."
-    exit 1
 fi
 
 if curl -s http://localhost:$APP_PORT | grep -q '200'; then
     echo "FastAPI app service is running."
 else
     echo "FastAPI app service is not responding. Check container logs."
-    exit 1
 fi
 
 if docker exec db pg_isready -U postgres &>/dev/null; then
     echo "PostgreSQL service is running."
 else
     echo "PostgreSQL service is not responding. Check container logs."
-    exit 1
 fi
