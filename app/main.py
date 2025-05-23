7a90a16657e7d971a11a72d4bffb5863213693f7
ronmaizel <ronmzl2003@gmail.com>
2025-05-23 20:45:22 +0300

    commit2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # type: ignore
 app = FastAPI()
 @app.get("/")
 def read_root():
--- start_trial.sh
+++ start_trial.sh
@@ -2,7 +2,7 @@
 
 # static vars
 REPO_URL="https://github.com/ronmaizel/DevOps_project.git"
-PROJECT_DIR="/opt/trial_plan"
+PROJECT_DIR="/opt/DevOps_project"
 NFS_service_IP="192.168.10.10"
 NFS_REMOTE_PATH="/media"
 NFS_MOUNT_PATH="/media"
@@ -11,39 +11,39 @@ WEB_PORT=80
 
 # install packages
 echo "Installing necessary packages."
-if ! command -v docker &> /dev/null; then
-    yum install curl
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sh get-docker.sh
-    sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-fi
+sudo dnf install curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+./get-docker.sh
+sudo dnf install -y docker-compose-plugin python3 python3-pip git nfs-common
+
+# add current user to docker group
+sudo usermod -aG docker $(whoami)
+
+# open relevant ports
+sudo firewall-cmd --add-port={80,5342,2049,8080}/tcp
 
 # clone git repo
 echo "Cloning project repository."
-if [ ! -d "$PROJECT_DIR" ]; then
-    cd /opt
-    git clone "$REPO_URL" "$PROJECT_DIR"
+cd /opt
+git clone "$REPO_URL"
+if [ -d $PROJECT_DIR ]; then
+  echo "Clone complete"
 fi
-
-cd "$PROJECT_DIR"
 
 # mount NFS
 echo "Mounting NFS share."
-sudo yum install -y nfs-common
 sudo mkdir -p "$NFS_MOUNT_PATH"
 sudo mount -t nfs "$NFS_service_IP:$NFS_REMOTE_PATH" "$NFS_MOUNT_PATH"
-
 echo "$NFS_service_IP:$NFS_REMOTE_PATH $NFS_MOUNT_PATH nfs defaults 0 0" >> /etc/fstab
 
-
 # start environment
+cd $PROJECT_DIR
 echo "Building and starting services."
 docker compose up -d
 echo "Trial environment ready."
 
 # health checks
 echo "Performing health checks."
-
 if curl -s http://localhost:$WEB_PORT | grep -q '200'; then
     echo "Nginx web service is running."
 else
@@ -64,4 +64,3 @@ else
     echo "PostgreSQL service is not responding. Check container logs."
     exit 1
 fi
-
